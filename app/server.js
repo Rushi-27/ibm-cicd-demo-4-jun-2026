ee0e9e7599d18bd8acbafa95374aba3a6f65f697
Rushikesh Malokar <rushikesh.malokar@ibm.com>
2026-07-16 10:21:47 +0530

    Changes in server.js

--- app/server.js
+++ app/server.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   console.log('done');
-  res.json({ status: 'UP' });
+  res.json({ status: 'UP' ,message: "Hi Rushikesh"});
 });
 
 // ESM equivalent of `if (require.main === module)` — only start the
